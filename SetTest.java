98eb4f89ffc61d13f42fbd34f965d8281eb54b9e
harledge <42972091+harledge@users.noreply.github.com>
2018-09-27 18:24:07 -0400

    Update SetTest.java

--- SetTest.java
+++ SetTest.java
@@ -7,7 +7,7 @@ import components.set.Set;
 /**
  * JUnit test fixture for {@code Set<String>}'s constructor and kernel methods.
  *
- * @author Put your name here
+ * @author Nik Anand and Hudson Arledge
  *
  */
 public abstract class SetTest {
