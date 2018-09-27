825a1efe78f949fa38b9dacad7b403920579f4e5
anand-92 <anand.92@osu.edu>
2018-09-27 16:12:57 -0400

    Update SetTest.java

--- SetTest.java
+++ SetTest.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import components.set.Set;
 
 /**
@@ -68,4 +72,83 @@ public abstract class SetTest {
 
     // TODO - add test cases for constructor, add, remove, removeAny, contains, and size
 
+    /*
+     * standard test case for add
+     */
+    @Test
+    public final void addTest1() {
+        Set<String> xExp = this.createFromArgsRef("one", "two");
+        Set<String> x = this.createFromArgsRef("one");
+        String addition = "two";
+        x.add(addition);
+
+        assertEquals(x, xExp);
+    }
+
+    /*
+     * standard test case for add (empty set)
+     */
+    @Test
+    public final void addTest2() {
+        Set<String> xExp = this.createFromArgsRef("one");
+        Set<String> x = this.createFromArgsRef();
+        String addition = "one";
+        x.add(addition);
+
+        assertEquals(x, xExp);
+    }
+
+    /*
+     * standard test case for add (another set)
+     */
+    @Test
+    public final void addTest3() {
+        Set<String> xExp = this.createFromArgsRef("one", "two", "three");
+        Set<String> x = this.createFromArgsRef("one");
+        Set<String> x2 = this.createFromArgsRef("two", "three");
+
+        x.add(x2);
+
+        assertEquals(x, xExp);
+    }
+
+    /*
+     * standard test case for remove (1 element)
+     */
+    @Test
+    public final void removeTest1() {
+        Set<String> xExp = this.createFromArgsRef();
+        Set<String> x = this.createFromArgsRef("one");
+
+        x.remove("one");
+
+        assertEquals(x, xExp);
+    }
+
+    /*
+     * standard test case for remove (2+ element)
+     */
+    @Test
+    public final void removeTest2() {
+        Set<String> xExp = this.createFromArgsRef("one", "two");
+        Set<String> x = this.createFromArgsRef("one", "two", "three");
+
+        x.remove("three");
+
+        assertEquals(x, xExp);
+    }
+
+    /*
+     * standard test case for remove (remove subset)
+     */
+    @Test
+    public final void removeTest3() {
+        Set<String> xExp = this.createFromArgsRef("three");
+        Set<String> x = this.createFromArgsRef("one", "two", "three");
+       
+        Set<String> xSubset = this.createFromArgsRef("one", "two");
+        x.remove(xSubset);
+
+        assertEquals(x, xExp);
+    }
 }
